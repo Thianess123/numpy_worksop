042b466be66433feabb700b9513e348cb4473c42
Thianess123 <thianess1234@gmail.com>
2024-05-22 11:10:25 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,10 @@
 #write a program to find the maximum of two numbers
+
+n1=int(input("enter two numbers:"))
+n2=int(input("enter two numbers:"))
+if n1>n2:
+    print("maximum is ",n1)
+elif n1==n2:
+    print("both are equal")
+else:
+    print("maximum is",n2)
